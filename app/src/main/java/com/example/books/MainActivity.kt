9a834d123a7bd79afd6727fff89883352d16b69c
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-11-05 21:08:26 +0000

    feat: Implemented BooksViewModel and Screens

--- app/src/main/java/com/example/books/MainActivity.kt
+++ app/src/main/java/com/example/books/MainActivity.kt
@@ -5,12 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.books.ui.BooksApp
 import com.example.books.ui.theme.BooksTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +16,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BooksTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    BooksApp()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BooksTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/example/books/ui/screens/HomeScreen.kt
@@ -0,0 +1,4 @@
+package com.example.books.ui.screens
+
+class HomeScreen {
+}
